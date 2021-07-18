ff91ad2298998ad5029ee6bc2f210d45f55ff0f9
Michael Buluma <buluma@users.noreply.github.com>
2021-07-18 09:39:52 +0300

    Update build-all.sh

--- build-all.sh
+++ build-all.sh
@@ -18,25 +18,25 @@ build_and_push(){
 	docker build --rm --force-rm -t "${REPO_URL}/${base}:${suite}" "${build_dir}" || return 1
 
 	# on successful build, push the image. NO PUSH!!
-	# echo "                       ---                                   "
-	# echo "Successfully built ${base}:${suite} with context ${build_dir}"
-	# echo "                       ---                                   "
+	echo "                       ---                                   "
+	echo "Successfully built ${base}:${suite} with context ${build_dir}"
+	echo "                       ---                                   "
 
-	# # try push a few times because notary server sometimes returns 401 for
-	# # absolutely no reason
-	# n=0
-	# until [ $n -ge 5 ]; do
-	# 	docker push --disable-content-trust=false "${REPO_URL}/${base}:${suite}" && break
-	# 	echo "Try #$n failed... sleeping for 15 seconds"
-	# 	n=$((n+1))
-	# 	sleep 15
-	# done
+	# try push a few times because notary server sometimes returns 401 for
+	# absolutely no reason
+	n=0
+	until [ $n -ge 5 ]; do
+		docker push --disable-content-trust=false "${REPO_URL}/${base}:${suite}" && break
+		echo "Try #$n failed... sleeping for 15 seconds"
+		n=$((n+1))
+		sleep 15
+	done
 
-	# # also push the tag latest for "stable" (chrome), "tools" (wireguard) or "3.5" tags for zookeeper
-	# if [[ "$suite" == "stable" ]] || [[ "$suite" == "3.6" ]] || [[ "$suite" == "tools" ]]; then
-	# 	docker tag "${REPO_URL}/${base}:${suite}" "${REPO_URL}/${base}:latest"
-	# 	docker push --disable-content-trust=false "${REPO_URL}/${base}:latest"
-	# fi
+	# also push the tag latest for "stable" (chrome), "tools" (wireguard) or "3.5" tags for zookeeper
+	if [[ "$suite" == "stable" ]] || [[ "$suite" == "3.6" ]] || [[ "$suite" == "tools" ]]; then
+		docker tag "${REPO_URL}/${base}:${suite}" "${REPO_URL}/${base}:latest"
+		docker push --disable-content-trust=false "${REPO_URL}/${base}:latest"
+	fi
 }
 
 dofile() {
