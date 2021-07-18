7c75f97af50fdbf267e4bf6e8f2fe6e2404cfdeb
ShadowBot <bulumaknight@gmail.com>
2021-07-18 12:27:11 +0300

    Update latest-versions.sh

--- latest-versions.sh
+++ latest-versions.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 # This script gets the latest GitHub releases for the specified projects.
 
+# if [[ -z "$GITHUB_TOKEN" ]]; then
+# 	echo "Set the GITHUB_TOKEN env variable."
+# 	exit 1
+# fi
+
+if [[ -n "$TOKEN" ]]; then
+	GITHUB_TOKEN=$TOKEN
+fi
+
 if [[ -z "$GITHUB_TOKEN" ]]; then
 	echo "Set the GITHUB_TOKEN env variable."
 	exit 1
